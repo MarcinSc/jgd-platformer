98b300ffe6456612d3f9689a134566bc7c614fe1
MarcinSc <marcins78@gmail.com>
2016-06-20 14:47:28 -0700

    Added FPSLogger and removed FPS limitations in DesktopLauncher.

--- core/src/main/java/jgd/platformer/Platformer.java
+++ core/src/main/java/jgd/platformer/Platformer.java
@@ -1,15 +1,19 @@
 package jgd.platformer;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.FPSLogger;
 
 public class Platformer extends ApplicationAdapter {
+    private FPSLogger fpsLogger;
 
     @Override
     public void create() {
+        fpsLogger = new FPSLogger();
     }
 
     @Override
     public void render() {
+        fpsLogger.log();
     }
 
     @Override
--- desktop/src/main/java/jgd/platformer/desktop/DesktopLauncher.java
+++ desktop/src/main/java/jgd/platformer/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import jgd.platformer.Platformer;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        // Only for debug purposes, remove for production
+        config.backgroundFPS = 0;
+        config.foregroundFPS = 0;
+        config.vSyncEnabled = false;
         new LwjglApplication(new Platformer(), config);
     }
 }
